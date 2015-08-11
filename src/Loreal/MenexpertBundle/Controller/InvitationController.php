89f3afd985da65ca5d5243faa04aad7cb35b4cf6
Spring.Zhou <spring.zhou@samesamechina.com>
2015-08-11 12:15:44 +0800

    no message

--- src/Loreal/MenexpertBundle/Controller/InvitationController.php
+++ src/Loreal/MenexpertBundle/Controller/InvitationController.php
@@ -8,26 +8,26 @@ class InvitationController extends Controller
 {
 	public function indexAction()
     {
-        return $this->render('LorealMenexpertBundle:Invatation:index.html.twig');
+        return $this->render('LorealMenexpertBundle:Invitation:index.html.twig');
     }
 
     public function errorAction()
     {
-        return $this->render('LorealMenexpertBundle:Invatation:error.html.twig');
+        return $this->render('LorealMenexpertBundle:Invitation:error.html.twig');
     }
 
     public function successAction()
     {
-        return $this->render('LorealMenexpertBundle:Invatation:success.html.twig');
+        return $this->render('LorealMenexpertBundle:Invitation:success.html.twig');
     }
 
     public function mainAction()
     {
-        return $this->render('LorealMenexpertBundle:Invatation:main.html.twig');
+        return $this->render('LorealMenexpertBundle:Invitation:main.html.twig');
     }
     
     public function interactionAction()
     {
-        return $this->render('LorealMenexpertBundle:Invatation:interaction.html.twig');
+        return $this->render('LorealMenexpertBundle:Invitation:interaction.html.twig');
     }
 }
