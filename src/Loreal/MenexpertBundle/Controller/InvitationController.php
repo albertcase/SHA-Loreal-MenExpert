d727bfd49b7f3b3df007edf45f195390d188b7c5
Demon Zhang <demon.zhang@samesamechina.com>
2015-08-11 12:02:37 +0800

    no message

---
+++ src/Loreal/MenexpertBundle/Controller/InvitationController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Loreal\MenexpertBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class InvitationController extends Controller
+{
+	public function indexAction()
+    {
+        return $this->render('LorealMenexpertBundle:Invatation:index.html.twig');
+    }
+
+    public function errorAction()
+    {
+        return $this->render('LorealMenexpertBundle:Invatation:error.html.twig');
+    }
+
+    public function successAction()
+    {
+        return $this->render('LorealMenexpertBundle:Invatation:success.html.twig');
+    }
+
+    public function mainAction()
+    {
+        return $this->render('LorealMenexpertBundle:Invatation:main.html.twig');
+    }
+}
