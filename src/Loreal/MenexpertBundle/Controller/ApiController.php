3b17b34fc83e07ebf122686888fa9487bce1f357
Demon Zhang <demon.zhang@samesamechina.com>
2015-08-13 14:43:58 +0800

    no message

--- src/Loreal/MenexpertBundle/Controller/ApiController.php
+++ src/Loreal/MenexpertBundle/Controller/ApiController.php
@@ -3,11 +3,30 @@
 namespace Loreal\MenexpertBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use LV\Bundle\CvdBundle\Entity\Locks;
 
-class DefaultController extends Controller
+class ApiController extends Controller
 {
-    public function indexAction($name)
+    public function checkAction()
     {
-        return $this->render('LorealMenexpertBundle:Default:index.html.twig', array('name' => $name));
+        $request = $this->getRequest()->request;
+        $vip = $this->getDoctrine()->getRepository('LorealMenexpertBundle:Vip')->findOneBy(array(
+		    'name' => $request->get('name') ,
+		    'mobile' => $request->get('mobile')
+		));
+		if(!$vip){
+			$url = $this->generateUrl('loreal_menexpert_error');
+			$status = array('status' => '2', 'url' => $url);
+	        $response = new JsonResponse();
+	        $response->setData($status);
+	        return $response;
+		}
+		$url = $this->generateUrl('loreal_menexpert_success');
+		$status = array('status' => '1', 'url' => $url);
+        $response = new JsonResponse();
+        $response->setData($status);
+        return $response;
+
     }
 }
---
+++ src/Loreal/MenexpertBundle/Entity/Vip.php
@@ -0,0 +1,124 @@
+<?php
+
+namespace Loreal\MenexpertBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Vip
+ *
+ * @ORM\Table()
+ * @ORM\Entity
+ */
+class Vip
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=100)
+     */
+    private $name;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="mobile", type="string", length=20)
+     */
+    private $mobile;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="created", type="string", length=100)
+     */
+    private $created;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return Vip
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string 
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set mobile
+     *
+     * @param string $mobile
+     * @return Vip
+     */
+    public function setMobile($mobile)
+    {
+        $this->mobile = $mobile;
+
+        return $this;
+    }
+
+    /**
+     * Get mobile
+     *
+     * @return string 
+     */
+    public function getMobile()
+    {
+        return $this->mobile;
+    }
+
+    /**
+     * Set created
+     *
+     * @param string $created
+     * @return Vip
+     */
+    public function setCreated($created)
+    {
+        $this->created = $created;
+
+        return $this;
+    }
+
+    /**
+     * Get created
+     *
+     * @return string 
+     */
+    public function getCreated()
+    {
+        return $this->created;
+    }
+}
