b335cb802e8ce949d47c4b101cecc12578dd4bed
Demon Zhang <demon.zhang@samesamechina.com>
2015-08-14 12:04:50 +0800

    no message

--- src/Loreal/MenexpertBundle/Controller/InteractionController.php
+++ src/Loreal/MenexpertBundle/Controller/InteractionController.php
@@ -8,8 +8,52 @@ class InteractionController extends Controller
 {
 	public function indexAction()
     {
-        echo 'This is Interaction Page';exit;
         return $this->render('LorealMenexpertBundle:Interaction:index.html.twig');
+    }
+
+    public function chooseAction()
+    {
+        return $this->render('LorealMenexpertBundle:Interaction:choose.html.twig');
+    }
+
+    public function photolistAction()
+    {
+        return $this->render('LorealMenexpertBundle:Interaction:photolist.html.twig');
+    }
+
+    public function mapAction()
+    {
+        return $this->render('LorealMenexpertBundle:Interaction:map.html.twig');
+    }
+
+    public function redAction()
+    {
+        return $this->render('LorealMenexpertBundle:Interaction:question.html.twig');
+    }
+
+    public function blueAction()
+    {
+        return $this->render('LorealMenexpertBundle:Interaction:blue.html.twig');
+    }
+
+    public function orangeAction()
+    {
+        return $this->render('LorealMenexpertBundle:Interaction:orange.html.twig');
+    }
+
+    public function purpleAction()
+    {
+        return $this->render('LorealMenexpertBundle:Interaction:purple.html.twig');
+    }
+
+    public function greenAction()
+    {
+        return $this->render('LorealMenexpertBundle:Interaction:green.html.twig');
+    }
+
+    public function resultAction()
+    {
+        return $this->render('LorealMenexpertBundle:Interaction:result.html.twig');
     }
 
 }
