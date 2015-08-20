7b4ec7bfe7cb7bee3eb6198c34b4e058367847cf
Spring.Zhou <spring.zhou@samesamechina.com>
2015-08-20 19:05:26 +0800

    add loading

---
+++ src/Loreal/MenexpertBundle/Resources/public/js/loading.js
@@ -0,0 +1,34 @@
+function LoadFn ( arr , fn , fn2){
+		var loader = new PxLoader();
+		for( var i = 0 ; i < arr.length; i ++)
+		{
+			loader.addImage(arr[i]);
+		};
+		
+		loader.addProgressListener(function(e) {
+				var percent = Math.round( e.completedCount / e.totalCount * 100 );
+				if(fn2) fn2(percent)
+		});	
+		
+		
+		loader.addCompletionListener( function(){
+			if(fn) fn();	
+		});
+		loader.start();	
+}
+
+
+
+function loading(allAmg, endFun){
+	LoadFn(allAmg , function (){
+		$(".loading").hide();
+		$(".photolist").animate({"opacity" : 1});
+		endFun();
+        console.log("加载完成!");
+	} , function ( p ){
+		//$('.loading').html(p+"%");
+	});
+
+
+}
+
