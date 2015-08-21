2418464e6b308f0c3683dfddc248ac4376ff2bb1
Demon Zhang <demon.zhang@samesamechina.com>
2015-08-21 17:14:19 +0800

    no message

---
+++ src/Loreal/MenexpertBundle/Entity/Photos.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace Loreal\MenexpertBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Photo
+ *
+ * @ORM\Table()
+ * @ORM\Entity
+ */
+class Photos
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="url", type="string", length=255)
+     */
+    private $url;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="created", type="string", length=100)
+     */
+    private $created;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set url
+     *
+     * @param string $url
+     * @return Photo
+     */
+    public function setUrl($url)
+    {
+        $this->url = $url;
+
+        return $this;
+    }
+
+    /**
+     * Get url
+     *
+     * @return string 
+     */
+    public function getUrl()
+    {
+        return $this->url;
+    }
+
+    /**
+     * Set created
+     *
+     * @param string $created
+     * @return Photo
+     */
+    public function setCreated($created)
+    {
+        $this->created = $created;
+
+        return $this;
+    }
+
+    /**
+     * Get created
+     *
+     * @return string 
+     */
+    public function getCreated()
+    {
+        return $this->created;
+    }
+}
--- src/Same/Bundle/WechatBundle/SameWechatBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Same\Bundle\WechatBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class SameWechatBundle extends Bundle
-{
-}
